0b43ebce55f3fdd02a8a9ec61151f611515f3422
yashas <yashashm@gmail.com>
2021-10-07 14:02:23 +0530

    modifications

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from API import app
 
 @app.route('/')
 def hello():
-    return '<h1>Hello World!</h1>'
+    return "<h1>Hello World!</h1>"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
